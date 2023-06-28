5e053b3a89158e0fe24311511cd305c213799d85
Rick Sanchez <ric137sanchez@proton.me>
2023-06-28 19:15:56 +0200

    Controlla di non estrarre immagini oltre la durata del video

--- src/images.py
+++ src/images.py
@@ -2,6 +2,8 @@ import os
 import sys
 
 import ffmpeg
+import pymediainfo
+import datetime
 
 from src import constants
 from src.api import imgur
@@ -10,10 +12,20 @@ from src.api import imgur
 def extract_screenshots(path: str, outputdir: str) -> dict:
     images = []
 
+    movie_ms = pymediainfo.MediaInfo.parse(path).tracks[0].duration
+
     with open(constants.screenshots, "r") as f:
         timecodes = f.read().splitlines()
 
         for time in timecodes:
+
+            t = datetime.datetime.strptime(time, "%H:%M:%S")
+            timecode_ms = (t.hour * 60 * 60 + t.minute * 60 + t.second) * 1000
+
+            # check if timecode is shorter than film duration
+            if timecode_ms > movie_ms:
+                break
+
             name = time.replace(":", ".") + ".png"
             generate_thumbnail(path, outputdir, name, time)
             images.append(os.path.join(outputdir, name))
